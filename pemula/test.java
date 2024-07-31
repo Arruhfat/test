8f79560b2945ccf17ef6c6d7d40f097d33a3ec69
Arruhfat <muhammad_arruhfath_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:50:18 +0700

    Add files via upload

---
+++ pemula/test.java
@@ -0,0 +1,10 @@
+public class test {
+public static void main(String[] args) {
+    System.out.println("Nama:M Arruhfat");
+    System.out.println("Alamat: JL Sanan gg12 No 102C");
+    System.out.println("Asal SMP5 MALANG");
+    System.out.println("NO HP :0895701283535");
+    
+
+} 
+}
